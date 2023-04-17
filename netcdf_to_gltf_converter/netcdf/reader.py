08d196308ef7255d352a15d7e06986d50c138f05
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-17 15:34:37 +0200

    #3: Add reader.py that can open a dataset and retrieve the mesh2d water depth

--- netcdf_to_gltf_converter/netcdf/reader.py
+++ netcdf_to_gltf_converter/netcdf/reader.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from pathlib import Path
+import xarray as xr
+import cf_xarray
 
 class StandardName(str, Enum):
     """Enum containg the valid variable standard names according to the 
@@ -15,4 +18,25 @@ class MeshType(str, Enum):
     """1D mesh"""
     mesh2d = "Mesh2d"
     """2D mesh"""
+        
+class Variable:
+    def __init__(self, data: xr.DataArray):
+        self._data = data
+        
+class Dataset:
+    def __init__(self, dataset: xr.Dataset) -> None:
+        self._ds = dataset
+
+    def get_water_depth_2d(self) -> Variable:
+        ds_mesh2d: xr.Dataset = self._ds.filter_by_attrs(mesh=MeshType.mesh2d)       
+        ds_water_depth: xr.DataArray = ds_mesh2d.cf[StandardName.water_depth]
+        
+        return Variable(data=ds_water_depth)
+
+class Reader:
+    def read(self, file: Path) -> Dataset:
+        ds: xr.Dataset = xr.open_dataset(file)
+        return Dataset(dataset=ds)
+
+        
         
