af046329d498963080f90efb775da12ccf2d6eb0
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-20 11:39:43 +0200

    #9: Add gltf exporter

---
+++ netcdf_to_gltf_converter/gltf/exporter.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from pygltflib import GLTF2
+
+class Exporter():
+    
+    @staticmethod
+    def export(gltf: GLTF2, file: Path):
+        """Export the GLTF object to file.
+        
+        If a file at the provided path already exists, it will be overwritten.
+
+        Args:
+            gltf (GLTF2): The GLTF object to export.
+            file (Path): The file path to export to.
+
+        Raises:
+            ValueError: When the file extension is not .gltf or .glb.
+        """
+        file_extension = file.suffix.lower()
+        if file_extension == ".gltf":
+            gltf.save(file) 
+        elif file_extension == ".glb":
+            gltf.save_binary(file)
+        else:
+            raise ValueError(f"GLTF file cannot be exported: unsupported file type '{file.suffix}'. Supported: .gltf, .glb")
---
+++ tests/utils.py
@@ -0,0 +1,19 @@
+from contextlib import contextmanager
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Generator
+
+
+@contextmanager
+def get_temp_file(filename: str) -> Generator[Path, None, None]:
+    """Gets a path to a file in a temporary directory with the specified file name.
+    Args:
+        filename (str): The file name.
+    Example:
+        >>>     with get_temp_file("some_file_name") as temp_file:
+        >>>         print(f"Do something with {temp_file}")
+    Yields:
+        Generator[Path, None, None]: Generator with the path to the file in the temporary directory as yield type.
+    """
+    with TemporaryDirectory() as temp_dir:
+        yield Path(temp_dir, filename)
