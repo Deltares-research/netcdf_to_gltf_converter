acdeccdfbb0b893a207162c0da8916513c2c62a2
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-24 12:30:16 +0200

    #3: Add interpolator for linear interpolation

--- netcdf_to_gltf_converter/interpolation/interpolator.py
+++ netcdf_to_gltf_converter/interpolation/interpolator.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import numpy as np
 from scipy import interpolate
 
@@ -5,6 +6,10 @@ from scipy import interpolate
 class Interpolator:
     """Class to interpolate data values onto a set of coordinates."""
 
+    class Method(str, Enum):
+        linear = "linear"
+        nearest = "nearest"
+        
     def interpolate_nearest(
         data_points: np.ndarray,
         data_values: np.ndarray,
@@ -23,5 +28,25 @@ class Interpolator:
             np.ndarray: The interpolated data values, an ndarray of floats with shape (m,).
         """
         return interpolate.griddata(
-            data_points, data_values, points_to_interpolate, method="nearest"
+            data_points, data_values, points_to_interpolate, method=Interpolator.Method.nearest
+        )
+
+    def interpolate_linear(
+        data_points: np.ndarray,
+        data_values: np.ndarray,
+        points_to_interpolate: np.ndarray,
+    ) -> np.ndarray:
+        """Interpolate the data values onto the points to interpolate.
+        Interpolation is performend by triangulating the input data, and on each triangle performing linear interpolation.
+        
+        Args:
+            data_points (np.ndarray): The data point coordinates, a 2D ndarray of floats with shape (n, 2).
+            data_values (np.ndarray): The data values, an ndarray of floats with shape (n,).
+            grid_points (np.ndarray): The points at which to interpolate data, a 2D ndarray of floats with shape (m, 2).
+
+        Returns:
+            np.ndarray: The interpolated data values, an ndarray of floats with shape (m,).
+        """
+        return interpolate.griddata(
+            data_points, data_values, points_to_interpolate, method=Interpolator.Method.linear
         )
--- tests/interpolation/test_interpolator.py
+++ tests/interpolation/test_interpolator.py
@@ -21,3 +21,21 @@ class TestInterpolator:
         exp_interpolated_values = [1, 1, 2, 3, 4, 2, 3, 4, 4]
 
         assert np.array_equal(interpolated_values, exp_interpolated_values)
+
+    def test_interpolate_linear(self):
+        data_points = np.array(
+            [[1.0,0.5], [2.0,1.0], [1.0,2.0], [0.5,1.0]], dtype="float32"
+        )
+        data_values = np.array([1, 2, 3, 4], dtype="float32")
+        points_to_interpolate = np.array(
+            [[0, 0], [1, 0], [2, 0], [0, 1], [1, 1], [2, 1], [0, 2], [1, 2], [2, 2]],
+            dtype="float32",
+        )
+
+        interpolated_values = Interpolator.interpolate_linear(
+            data_points, data_values, points_to_interpolate
+        )
+
+        exp_interpolated_values = [float("nan"), float("nan"), float("nan"), float("nan"), 1.6666666666666665, 2.0, float("nan"), 3.0, float("nan")]
+
+        assert np.array_equal(interpolated_values, exp_interpolated_values, equal_nan=True)
