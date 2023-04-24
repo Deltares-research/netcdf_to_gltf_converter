3085c3ca6a58088403127af4d946c732f94e0b7b
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-24 13:23:44 +0200

    #3: Move creation of rectilinear grid in test to factory class.

--- tests/preprocessing/test_triangulation.py
+++ tests/preprocessing/test_triangulation.py
@@ -3,32 +3,11 @@ import xugrid as xu
 from xugrid import Ugrid2d
 
 from netcdf_to_gltf_converter.preprocessing.triangulation import Triangulator
+from tests.preprocessing.utils import Factory
 
-def create_rectilinear_ugrid2d() -> xu.Ugrid2d:
-    """
-    5--6--7
-    |  |  |
-    3--4--5
-    |  |  |
-    0--1--2
-
-    """ 
-    node_x = np.array([0,1,2,0,1,2,0,1,2])
-    node_y = np.array([0,0,0,1,1,1,2,2,2])
-    fill_value= -1
-    face_node_connectivity = np.array([
-        [0, 1, 4, 3],
-        [1, 2, 5, 4],
-        [3, 4, 7, 6],
-        [4, 5, 8, 7]
-    ])
-
-    return xu.Ugrid2d(node_x, node_y, fill_value, face_node_connectivity)
-        
-        
 class TestTriangulator:
     def test_triangulate(self):
-        rectilinear_grid = create_rectilinear_ugrid2d()
+        rectilinear_grid = Factory.create_rectilinear_ugrid2d()
         triangulated_grid = Triangulator.triangulate(rectilinear_grid)
         
         assert np.array_equal(rectilinear_grid.node_x, triangulated_grid.node_x)
