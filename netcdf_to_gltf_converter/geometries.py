bde246ed2b40eb20dd5c80aa0acb418078dd0b7e
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-26 23:23:00 +0200

    #21: Cleanup, add docs

--- netcdf_to_gltf_converter/geometries.py
+++ netcdf_to_gltf_converter/geometries.py
@@ -3,53 +3,51 @@ from typing import List
 import numpy as np
 
 
-def validate_array(array: np.ndarray, dtype: str, n_shape: int, n_col: int):
+def validate_2d_array(array: np.ndarray, dtype: str, n_col: int):
     assert array.dtype == dtype
-    assert len(array.shape) == n_shape
+    assert len(array.shape) == 2
     assert array.shape[1] == n_col
 
 
 class MeshGeometry:
-    def __init__(
-        self,
-        vertex_positions: np.ndarray,
-    ) -> None:
-        """_summary_
+    def __init__(self, vertex_positions: np.ndarray) -> None:
+        """Initialize a MeshGeometry with the specified arguments.
 
         Args:
-            vertex_positions (np.ndarray (n, 3)): float32
+            vertex_positions (np.ndarray): The vertex positions, an ndarray of floats with shape (n, 3). Each row represents one vertex and contains the x, y and z coordinate of this vertex.
+        
+        Raises: 
+            AssertionError: When the shape or dtype of the `vertex_positions` does not match the requirements.
         """
         self.vertex_positions = vertex_positions
         self.validate()
 
     def validate(self):
-        validate_array(self.vertex_positions, "float32", n_shape=2, n_col=3)
+        validate_2d_array(self.vertex_positions, "float32", n_col=3)
 
 
 class TriangularMesh:
-    def __init__(
-        self,
-        mesh_geometry: MeshGeometry,
-        triangles: np.ndarray,
-        mesh_transformations: List[MeshGeometry],
-    ) -> None:
-        """Initialize a TriangularMesh with the given arguments.
+    def __init__(self, mesh_geometry: MeshGeometry, triangles: np.ndarray, mesh_transformations: List[MeshGeometry]) -> None:
+        """Initialize a TriangularMesh with the specified arguments.
 
         Args:
-            mesh_geometry (MeshGeometry): The nodes in the mesh.
-            triangles (np.ndarray (m, 3)): uint32The triangles in the mesh each containing the three node indices that define the triangle shape and position.
-            mesh_transformations (List[MeshGeometry]): The collection of node transformations.
+            mesh_geometry (MeshGeometry): The base mesh geometry.
+            triangles (np.ndarray): The vertex indices per triangle, an ndarray of integers with shape (m, 3). Each row represents one triangle and contains the three vertex indices of this triangle.
+            mesh_transformations (List[MeshGeometry]): The mesh transformations containing the vertex displacements. 
+        
+        Raises: 
+            AssertionError: When the shape or dtype of the `triangles` does not match the requirements.
+            AssertionError: When the size of the vertex positions in any of the mesh transformations do not match the amount in the base mesh geometry.
         """
+        
         self.mesh_geometry = mesh_geometry
         self.triangles = triangles
         self.mesh_transformations = mesh_transformations
         self.validate()
 
     def validate(self):
-        validate_array(self.triangles, "uint32", n_shape=2, n_col=3)
-        base_mesh_geometry_node_size = self.mesh_geometry.vertex_positions.size
+        validate_2d_array(self.triangles, "uint32", n_col=3)
+        
+        base_vertex_size = self.mesh_geometry.vertex_positions.size
         for mesh_transformation in self.mesh_transformations:
-            assert (
-                mesh_transformation.vertex_positions.size
-                == base_mesh_geometry_node_size
-            )
+            assert mesh_transformation.vertex_positions.size == base_vertex_size
