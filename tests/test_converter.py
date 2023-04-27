6e3fc9baf77a471373b83d90686d4a6d1ee70b33
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-27 22:28:55 +0200

    #26: Add converter test

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 
 from netcdf_to_gltf_converter.converter import Converter
-
+from tests.utils import assert_files_equal, get_temp_file, resources, reference_files
 
 class TestConverter:
     def test_initialize_netcdf_does_not_exist_raises_error(self):
@@ -14,3 +14,13 @@ class TestConverter:
             _ = Converter(netcdf, gltf)
 
         assert str(error.value) == rf"NetCDF file does not exist: {netcdf}"
+
+    def test_run(self):
+        netcdf_file_path = resources / "3x3nodes_rectilinear_map.nc"
+        reference_gltf_file_path = reference_files / "3x3nodes_rectilinear_map.gltf"
+        
+        with get_temp_file(reference_gltf_file_path.name) as gltf_file_path:
+            converter = Converter(netcdf_file_path, gltf_file_path)
+            converter.run()
+            
+            assert_files_equal(gltf_file_path, reference_gltf_file_path)
