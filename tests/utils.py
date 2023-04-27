d30cf6fbfa1829a8147ced3fd983ffcd29b91176
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-27 22:27:46 +0200

    #26: Add file comparison to utils, add path to reference files.

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,23 @@
 from contextlib import contextmanager
+import filecmp
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Generator
 
 resources = Path("tests") / "resources"
+reference_files = resources / "reference"
 
+def assert_files_equal(file_1: Path, file_2: Path):
+    """Assert whether the contents of two files are equal.
+
+    Args:
+        file_1 (Path): The path of the first file.
+        file_2 (Path): The path of the second file.
+
+    Raises:
+        AssertionError: When the content of the two files are not equal.
+    """
+    assert filecmp.cmp(str(file_1), str(file_2)) == True
 
 @contextmanager
 def get_temp_file(filename: str) -> Generator[Path, None, None]:
