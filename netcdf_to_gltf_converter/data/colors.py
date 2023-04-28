24fc5b156479113d281a0747ea9d7be4f6c6d77c
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-28 15:10:23 +0200

    #30: Add plane mesh color

--- netcdf_to_gltf_converter/data/colors.py
+++ netcdf_to_gltf_converter/data/colors.py
@@ -1,2 +1,5 @@
 DEFAULT_MESH_COLOR = [0.38, 0.73, 0.78, 1.0]
 """The default vertex color in the mesh defined by the normalized red, green, blue and alpha (RGBA) values."""
+
+PLANE_MESH_COLOR = [1.0, 1.0, 1.0, 1.0] 
+"""The vertex color in the plane mesh defined by the normalized red, green, blue and alpha (RGBA) values."""
