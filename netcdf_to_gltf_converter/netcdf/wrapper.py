f80e4c3a5a5bed59ec3355368be5bf8806c6d8c3
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-28 20:48:44 +0200

    #26: Make interpolator non-static

--- netcdf_to_gltf_converter/netcdf/wrapper.py
+++ netcdf_to_gltf_converter/netcdf/wrapper.py
@@ -57,18 +57,16 @@ class StandardName(str, Enum):
     water_depth = "sea_floor_depth_below_sea_surface"
     """The vertical distance between the sea surface and the seabed as measured at a given point in space including the variance caused by tides and possibly waves."""
 
-
 class Wrapper:
-    @staticmethod
-    def interpolate(data_coords: np.ndarray, data_values: np.ndarray, grid):
-        return Interpolator.interpolate_nearest(
-            data_coords, data_values, grid, Location.nodes
-        )
-
+    
     def __init__(self, dataset: xr.Dataset) -> None:
         self._dataset = dataset
         self._grid = Ugrid2d.from_dataset(dataset, MeshType.mesh2d)
+        self._interpolator = Interpolator()
 
+    def _interpolate(self, data_coords: np.ndarray, data_values: np.ndarray, grid: Ugrid2d):
+        return self._interpolator.interpolate_nearest(data_coords, data_values, grid, Location.nodes)
+        
     def _get_coordinates(self, location: DataLocation):
         if location == DataLocation.face:
             return self._grid.face_coordinates
@@ -107,20 +105,17 @@ class Wrapper:
         data_values = data.isel(time=0)
 
         triangulated_grid = Triangulator.triangulate(self._grid)
-        interpolated_vertex_positions = Wrapper.interpolate(
-            data_coords, data_values, triangulated_grid
-        )
-        base_geometry = MeshAttributes(vertex_positions=interpolated_vertex_positions)
+        interpolated_data = self._interpolate(data_coords, data_values, triangulated_grid)
+        
+        base_geometry = MeshAttributes(vertex_positions=interpolated_data)
 
         transformations: List[MeshAttributes] = []
         n_times = data.sizes["time"]
         for time_index in range(1, n_times):
             data_values = data.isel(time=time_index)
-            interpolated_vertex_positions = Wrapper.interpolate(
-                data_coords, data_values, triangulated_grid
-            )
+            interpolated_data = self._interpolate(data_coords, data_values, triangulated_grid)
             vertex_displacements = np.subtract(
-                interpolated_vertex_positions,
+                interpolated_data,
                 base_geometry.vertex_positions,
                 dtype=np.float32,
             )
