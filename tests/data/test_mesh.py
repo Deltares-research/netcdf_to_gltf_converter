07e5fe28794b2444fbcd6a7b7dcc74c58292ac70
priscavdsluis <priscavdsluis@users.noreply.github.com>
2023-05-15 11:04:07 +0000

    autoformat: isort & black

--- tests/data/test_mesh.py
+++ tests/data/test_mesh.py
@@ -90,7 +90,9 @@ class TestTriangularMesh:
             ]
         )
 
-        threshold_mesh = triangular_mesh.get_threshold_mesh(height=0.01, color=[1.0, 1.0, 1.0, 1.0])
+        threshold_mesh = triangular_mesh.get_threshold_mesh(
+            height=0.01, color=[1.0, 1.0, 1.0, 1.0]
+        )
 
         assert np.array_equal(threshold_mesh.triangles, triangular_mesh.triangles)
         assert np.array_equal(
