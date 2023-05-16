9ecff23e6f0f4d16341696c294a14c92108b2627
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-05-16 10:40:31 +0200

    #39: Move a lot from parser to wrapper

--- netcdf_to_gltf_converter/netcdf/parser.py
+++ netcdf_to_gltf_converter/netcdf/parser.py
@@ -1,4 +1,4 @@
-from enum import Enum
+
 from typing import Generator
 
 import numpy as np
@@ -7,18 +7,11 @@ from xugrid import Ugrid2d
 
 from netcdf_to_gltf_converter.config import Config
 from netcdf_to_gltf_converter.data.mesh import MeshAttributes, TriangularMesh
-from netcdf_to_gltf_converter.netcdf.conventions import AttrKey, AttrValue
+from netcdf_to_gltf_converter.netcdf.wrapper import Wrapper
 from netcdf_to_gltf_converter.preprocessing.interpolation import Interpolator, Location
 from netcdf_to_gltf_converter.preprocessing.transformation import Transformer
 from netcdf_to_gltf_converter.preprocessing.triangulation import Triangulator
 from netcdf_to_gltf_converter.utils.arrays import uint32_array
-
-
-class DataLocation(str, Enum):
-    face = "face"
-    node = "node"
-    edge = "edge"
-
 
 class Parser:
     def __init__(self, dataset: xr.Dataset, config: Config) -> None:
@@ -32,11 +25,11 @@ class Parser:
         self._interpolator = Interpolator()
         self._triangulator = Triangulator()
         self._tranformer = Transformer(dataset, config)
-
+        self._wrapper = Wrapper(dataset, config)
+        
         self._dataset = dataset
         self._config = config
-        self._2d_topology = self._get_2d_topology()
-        self._grid = Ugrid2d.from_dataset(dataset, self._2d_topology)
+
 
 
     def _interpolate(
@@ -46,36 +39,7 @@ class Parser:
             data_coords, data_values, grid, Location.nodes
         )
 
-    def _get_coordinates(self, location: DataLocation) -> np.ndarray:
-        if location == DataLocation.face:
-            return self._grid.face_coordinates
-        if location == DataLocation.edge:
-            return self._grid.edge_coordinates
-        if location == DataLocation.node:
-            return self._grid.node_coordinates
 
-        raise ValueError(f"Location {location} not supported.")
-
-    def _get_variables_by_attr_filter(self, **filter):
-        dataset = self._dataset.filter_by_attrs(**filter)
-        for variable in dataset.values():
-            yield variable
-
-    def _get_2d_topology(self) -> str:
-        attr_filter = {
-            AttrKey.cf_role: AttrValue.mesh_topology,
-            AttrKey.topology_dimension: 2,
-        }
-        variable = next(self._get_variables_by_attr_filter(**attr_filter))
-        return variable.name
-
-    def _get_2d_variable(self, standard_name: str) -> xr.DataArray:
-        attr_filter = {
-            AttrKey.standard_name: standard_name,
-            AttrKey.mesh: self._2d_topology,
-        }
-        variable = next(self._get_variables_by_attr_filter(**attr_filter))
-        return variable
 
     def _get_transformations(
         self,
@@ -98,9 +62,8 @@ class Parser:
             yield MeshAttributes(vertex_positions=vertex_displacements)
 
     def to_triangular_mesh(self, standard_name: str):
-        data = self._get_2d_variable(standard_name)
-        data_location = data.attrs.get(AttrKey.location)
-        data_coords = self._get_coordinates(data_location)
+        data = self._wrapper.get_2d_variable(standard_name)
+        data_coords = self._wrapper.get_data_coordinates(data)
         data_values = data.isel(time=0).to_numpy()
 
         self._config.shift_coordinates = True
@@ -110,7 +73,7 @@ class Parser:
         if self._config.scale != 1.0:
             self._scale_coordinates(data_coords, data_values)
 
-        triangulated_grid = self._triangulator.triangulate(self._grid)
+        triangulated_grid = self._triangulator.triangulate(self._wrapper.grid)
         interpolated_data = self._interpolate(
             data_coords, data_values, triangulated_grid
         )
@@ -128,19 +91,19 @@ class Parser:
         )
 
     def _shift_coordinates(self, coordinates: np.ndarray):
-        shift_x = self._grid.node_x.min()
-        shift_y = self._grid.node_y.min()
+        shift_x = self._wrapper.grid.node_x.min()
+        shift_y = self._wrapper.grid.node_y.min()
 
         coordinates[:, 0] -= shift_x
         coordinates[:, 1] -= shift_y
 
-        self._grid.node_x -= shift_x
-        self._grid.node_y -= shift_y
+        self._wrapper.grid.node_x -= shift_x
+        self._wrapper.grid.node_y -= shift_y
 
     def _scale_coordinates(self, coordinates: np.ndarray, data_values: np.ndarray):
         scale = self._config.scale
 
         np.multiply(coordinates, scale, out=coordinates)
         np.multiply(data_values, scale, out=data_values)
-        np.multiply(self._grid.node_x, scale, out=self._grid.node_x)
-        np.multiply(self._grid.node_y, scale, out=self._grid.node_y)
+        np.multiply(self._wrapper.grid.node_x, scale, out=self._wrapper.grid.node_x)
+        np.multiply(self._wrapper.grid.node_y, scale, out=self._wrapper.grid.node_y)
