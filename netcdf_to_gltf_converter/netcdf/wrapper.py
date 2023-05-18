08d91341f0c82fbde25a483c47031ba767c70f91
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-05-18 23:00:12 +0200

    #54: Remove unused methods.

--- netcdf_to_gltf_converter/netcdf/wrapper.py
+++ netcdf_to_gltf_converter/netcdf/wrapper.py
@@ -8,9 +8,7 @@ import xugrid as xu
 from netcdf_to_gltf_converter.config import Config
 from netcdf_to_gltf_converter.netcdf.conventions import (
     AttrKey,
-    CfRoleAttrValue,
-    LocationAttrValue,
-)
+    CfRoleAttrValue)
 
 
 class Topology(str, Enum):
@@ -134,18 +132,6 @@ class UgridDataset:
         """
         return self._get_coord_vars_for_topology(Topology.faces)
 
-    def get_data_coordinates(self, data: xr.DataArray) -> np.ndarray:
-        """Get the coordinates for the provided data.
-
-        Args:
-            data (xr.DataArray): The data to get the coordinates for.
-
-        Returns:
-            np.ndarray: A 2D np.ndarray of floats with shape (n, 2) where each row contains a x and y coordinate.
-        """
-        location = data.attrs.get(AttrKey.location)
-        return self._get_coordinates_for_location(location)
-
     def set_variable(self, variable: xr.DataArray):
         """Update the variable in the data set.
 
@@ -169,7 +155,7 @@ class UgridDataset:
             variable_name (str): The variable name.
 
         Returns:
-            xr.DataArray: An xr.DataArray containing the variable data.
+            UgridVariable: A UgridVariable.
 
         Raises:
             ValueError: When the dataset does not contain a variable with the name.
@@ -195,23 +181,6 @@ class UgridDataset:
             )
 
         return self._dataset[variable_name]
-
-    def _get_coordinates_for_location(self, location: LocationAttrValue) -> np.ndarray:
-        x_coord_var, y_coord_var = self._get_coord_vars_for_location(location)
-        x_coords = x_coord_var.values
-        y_coords = y_coord_var.values
-        coords = np.column_stack([x_coords, y_coords])
-
-        return coords
-
-    def _get_coord_vars_for_location(self, location: LocationAttrValue) -> np.ndarray:
-        if location == LocationAttrValue.node:
-            return self.node_coord_vars
-        if location == LocationAttrValue.edge:
-            return self.edge_coord_vars
-        if location == LocationAttrValue.face:
-            return self.face_coord_vars
-        raise ValueError(f"Location {location} not supported.")
 
     def _get_coord_vars_for_topology(self, location: Topology) -> Tuple:
         var_names = self._topologies[location]
